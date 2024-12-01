5f7f3ea8f629f3c675d895a60c712cb2b1d366ba
iamdanunah <serhiidemchev@gmail.com>
2024-12-01 23:12:45 +0200

    !

--- index.php
+++ index.php
@@ -144,26 +144,23 @@ echo '</div>';
 include_once 'foot.php';
 ?>
 <script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const ENButton = document.getElementById("ENbutton");
-        const UAButton = document.getElementById("UAbutton");
-        const ENForm = document.getElementById("ENform");
-        const UAForm = document.getElementById("UAform");
+document.addEventListener("load", function() {
+    const ENButton = document.getElementById("ENbutton");
+    const UAButton = document.getElementById("UAbutton");
+    const ENForm = document.getElementById("ENform");
+    const UAForm = document.getElementById("UAform");
 
-        // Показуємо EN форму
-        ENButton.addEventListener("click", function () {
-            ENForm.style.display = "block";
-            UAForm.style.display = "none";
-        });
-
-        // Показуємо UA форму
-        UAButton.addEventListener("click", function () {
-            ENForm.style.display = "none";
-            UAForm.style.display = "block";
-        });
-
-        // Початковий стан
-        ENForm.style.display = "block"; // або "none", якщо потрібно стартувати з прихованих форм
+    ENButton.addEventListener("click", function () {
+        ENForm.style.display = "block";
         UAForm.style.display = "none";
     });
+
+    UAButton.addEventListener("click", function () {
+        ENForm.style.display = "none";
+        UAForm.style.display = "block";
+    });
+
+    ENForm.style.display = "block";
+    UAForm.style.display = "none";
+});
 </script>
